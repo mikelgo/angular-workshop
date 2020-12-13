9f0c6046013d16428e3d0df14f82d868b66fec90
Gregor Woiwode <gregor.woiwode@gmail.com>
2020-12-13 21:56:39 +0100

    solve--use-lazy-loading-for-BookModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { AppComponent } from './app.component';
-import { BookDetailComponent } from './book/book-detail/book-detail.component';
-import { BookComponent } from './book/book.component';
 
 const routes: Routes = [
   {
@@ -13,11 +11,8 @@ const routes: Routes = [
   },
   {
     path: 'books',
-    component: BookComponent
-  },
-  {
-    path: 'books/details/:isbn',
-    component: BookDetailComponent
+    loadChildren: () =>
+      import('./book/book.module').then(module => module.BookModule)
   },
   {
     path: 'about',
