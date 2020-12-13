db10a9a97bace436d3d8e414527567b89d810500
Gregor Woiwode <gregor.woiwode@gmail.com>
2020-12-13 19:24:17 +0100

    solve--load-data-from-local-api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
@@ -5,7 +6,7 @@ import { BookModule } from './book/book.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BookModule],
+  imports: [BrowserModule, HttpClientModule, BookModule],
   providers: [],
   bootstrap: [AppComponent]
 })
