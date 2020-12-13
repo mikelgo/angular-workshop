dc4dcae1a4f452c9581384955a007309a39d9a00
Gregor Woiwode <gregor.woiwode@gmail.com>
2020-12-13 21:33:16 +0100

    solve--add-BookDetail-route

--- src/app/book/book-na.ts
+++ src/app/book/book-na.ts
@@ -3,7 +3,9 @@ import { Book } from './book';
 export class BookNa implements Book {
   #na = 'n/a';
 
-  title = this.#na;
   abstract = this.#na;
   author = this.#na;
+  cover = this.#na;
+  isbn = this.#na;
+  title = this.#na;
 }
--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BookCardComponent } from './book-card/book-card.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
 import { BookFilterPipe } from './book-filter/book-filter.pipe';
 import { BookComponent } from './book.component';
 
 @NgModule({
-  declarations: [BookComponent, BookCardComponent, BookFilterPipe],
+  declarations: [
+    BookComponent,
+    BookCardComponent,
+    BookFilterPipe,
+    BookDetailComponent
+  ],
   imports: [CommonModule],
   exports: [BookComponent]
 })
