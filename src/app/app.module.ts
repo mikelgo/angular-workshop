7d773bea97092647897a7058e3cbe9d4c9ab2569
Gregor Woiwode <gregor.woiwode@gmail.com>
2020-12-13 13:26:53 +0100

    solve--your-1st-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BookCardComponent } from './book-card/book-card.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BookCardComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
