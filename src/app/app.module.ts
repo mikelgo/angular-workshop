99c53cfea33de435033dc18f485ae940b1286ab5
Gregor Woiwode <gregor.woiwode@gmail.com>
2020-12-13 19:01:42 +0100

    solve--create-a-BookModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { BookCardComponent } from './book-card/book-card.component';
-import { BookFilterPipe } from './book-filter/book-filter.pipe';
+import { BookModule } from './book/book.module';
 
 @NgModule({
-  declarations: [AppComponent, BookCardComponent, BookFilterPipe],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BookModule],
   providers: [],
   bootstrap: [AppComponent]
 })
