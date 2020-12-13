5dc2be1272280adff532cbbfc4a6fce4ed4529cb
Gregor Woiwode <gregor.woiwode@gmail.com>
2020-12-13 13:50:55 +0100

    solve--maken-an-input-binding-typesafe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Book } from './book';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  book = {
+  book: Book = {
     title: 'How to win friends',
     author: 'Dale Carnegie',
     abstract:
